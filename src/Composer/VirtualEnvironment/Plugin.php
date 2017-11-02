6dfdf63bc3409360252d76c27c9a72b1da64235e
Stephan Jorek <stephan.jorek@gmail.com>
2017-11-02 17:13:17 +0100

    [FEATURE] remove obsolete shell-activator hook implementation

--- src/Composer/VirtualEnvironment/Plugin.php
+++ src/Composer/VirtualEnvironment/Plugin.php
@@ -18,7 +18,6 @@ use Composer\Plugin\Capable;
 use Composer\Plugin\Capability\CommandProvider;
 use Sjorek\Composer\VirtualEnvironment\Command\GitHookCommand;
 use Sjorek\Composer\VirtualEnvironment\Command\ShellActivatorCommand;
-use Sjorek\Composer\VirtualEnvironment\Command\ShellActivatorHookCommand;
 use Sjorek\Composer\VirtualEnvironment\Command\ShellHookCommand;
 use Sjorek\Composer\VirtualEnvironment\Command\SymbolicLinkCommand;
 
@@ -83,7 +82,6 @@ class Plugin implements PluginInterface, Capable, CommandProvider
     {
         return array(
             new ShellActivatorCommand(null, $this->composer, $this->io),
-            new ShellActivatorHookCommand(null, $this->composer, $this->io),
             new ShellHookCommand(null, $this->composer, $this->io),
             new GitHookCommand(null, $this->composer, $this->io),
             new SymbolicLinkCommand(null, $this->composer, $this->io),
